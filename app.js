4c2aad14def0a25d1108e9e245ad44c7a15e62b4
Ramim <191146698+Ramim597@users.noreply.github.com>
2026-02-12 11:27:33 +0600

    color flipper

---
+++ app.js
@@ -0,0 +1,30 @@
+let btn = document.getElementById("change-color-btn");
+
+btn.addEventListener("click", () => {
+  let colors = [
+    "#FF5733",
+    "#33FF57",
+    "#3357FF",
+    "#F1C40F",
+    "#9B59B6",
+    "#1ABC9C",
+    "#E74C3C",
+    "#2ECC71",
+    "#3498DB",
+    "#E67E22",
+    "#34495E",
+    "#16A085",
+    "#8E44AD",
+    "#C0392B",
+    "#27AE60",
+    "#2980B9",
+    "#D35400",
+    "#7F8C8D",
+    "#2C3E50",
+    "#F39C12",
+  ];
+  let random = Math.random() * 3;
+  let randomColors = Math.floor(random);
+  let selectedColor = colors[randomColors];
+  document.body.style.background = selectedColor;
+});
